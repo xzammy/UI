b9c9243b8108e89024c0325b33bb2f9e7ca8348e
Cameron <xwhalex@icloud.com>
2019-09-19 00:53:25 +0800

    Theme Changes

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@ var $$ = Dom7;
 var app  = new Framework7({
   root: '#app', // App root element
   id: 'io.framework7.testapp', // App bundle ID
-  name: 'Framework7', // App name
+  name: 'AI Nurse', // App name
   theme: 'auto', // Automatic theme detection
   // App root data
   data: function () {
